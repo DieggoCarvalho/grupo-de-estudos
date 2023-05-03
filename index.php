d3a28c3c52004fff6b5a9e66777110d1fd68d96b
Dieggo <45542233+DieggoCarvalho@users.noreply.github.com>
2023-05-03 00:14:52 -0300

    ajustes

--- index.php
+++ index.php
@@ -5,8 +5,11 @@
     <link rel="stylesheet" href="assets/css/style.css" />
     <meta name="viewport" content="width=device-width, user-scalable=0" />
     <title>Grupo de Estudos</title>
+    <meta http-equiv="refresh" content="15" />
 </head>
 <body>
+    <!-- topo -->
+    <!-- navbar -->
     <header>
         <div class="container">
             <div id="logotipo" class="header__item">GRUPO DE ESTUDOS</div>
@@ -15,14 +18,24 @@
                     <li class="header__item"><a href="./">HOME</a></li>
                     <li class="header__item"><a href="">SOBRE</a></li>
                     <li class="header__item"><a href="">PROJETOS</a></li>
-                    <li class="header__item"><a href="">USUÁRIO</a></li>
+                    <li class="header__item"><a href="">LOGIN</a></li>
                 </ul>
             </nav>
         </div>
     </header>
     <div class="a-header__line"></div>
+    <!-- banner -->
     <article id="banner">
-        
+        <div class="container">
+            <div class="banner__item b-left">
+                <div class="banner__frase1">UM GRUPO QUE<br>PODE DAR CERTO!</div>
+                <div class="banner__frase2">Se todo mundo ajudar.</div>
+                <button class="banner__btn">PARTICIPE</button>
+            </div>
+            <div class="banner__item b-right">
+                <img src="assets/img/banner.png" alt="grupo-de-estudos" />
+            </div>
+        </div>
     </article>
     <main>
 
